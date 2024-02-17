09a091ab5bdab20c84186738869a8baeda71b4fd
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-02-17 14:20:08 -0600

    Update with final add on

--- Oops.cpp
+++ Oops.cpp
@@ -44,7 +44,7 @@ void take_input(int lowerbound, int upperbound) {
     int Userinput, helper = 1;
     string stringinput;
 
-    cout << "\nPlease enter a number " << lowerbound << " to " << upperbound << ":  ";
+    cout << "\nPlease enter a number " << lowerbound << " to " << upperbound << ", or type help for options.\n";
     cin >> stringinput;
 
     if (isdigit(stringinput[0])) {
@@ -53,13 +53,22 @@ void take_input(int lowerbound, int upperbound) {
     }
     else {
         if (stringinput == "exit") {
-            cout << "goodbye...";
+            cout << "Exiting now. Goodbye...";
         }
         else if (stringinput == "default") {
             helper = check_input(lowerbound, upperbound, 100);
         }
-        else {
+        else if (stringinput == "help") {
             helper = 2;
+            cout << "Type in a number to see if it is within the upper and lower bound\n"
+                << "Type in a string to return a string.\n"
+                << "Type in exit to leave program.\n"
+                << "Type in default to put in a value of 100\n";
+        }
+        else {
+            string idk;
+            getline(cin, idk);
+            cout << "The value chosen by the user is " << stringinput << idk;
         }
     }
 
@@ -68,16 +77,16 @@ void take_input(int lowerbound, int upperbound) {
     }
 
 }
- 
+
 //tests the different functions
 void Driver() {
     cout << "starting test..." << endl;
     cout << "\ttesting set values..." << endl;
-    int value1=0, value2=0;
+    int value1 = 0, value2 = 0;
     tie(value1, value2) = set_values(1);
-    assert(value1== 1);
+    assert(value1 == 1);
     assert(value2 == 100);
-    
+
     cout << "\ttesting check input..." << endl;
     value1 = check_input(1, 10, 2);
     assert(value1 = 1);
@@ -96,9 +105,9 @@ void Driver() {
 //main program
 int main()
 {
-    int Userinput=1;
+    int Userinput = 1;
     char YorN;
-    int upperbound,lowerbound;
+    int upperbound, lowerbound;
 
     cout << "Do you want to enter a lower and upper bounds (Yes or No)?  ";
     cin >> YorN;
@@ -113,9 +122,9 @@ int main()
     else {
         tie(lowerbound, upperbound) = set_values(1);
     }
-    
+
     take_input(lowerbound, upperbound);
-    
-    
+
+
     return 0;
 }
